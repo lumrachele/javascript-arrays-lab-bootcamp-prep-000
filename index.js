48e7c2fa1e3e1b04d9c260ffb8895029db2bc5de
Rachel Lum <rlum894@gmail.com>
2018-09-29 21:22:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyAppendKitten (name) {
 
 function destructivelyPrependKitten(name) {
   return kittens.unshift(`${name}`)
-  }
+}
 
 //function destructivelyRemoveLastKitten() {
   
